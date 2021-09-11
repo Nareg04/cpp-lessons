5edad27f8fd5622fe2f482702d9ac2ee8de3c1d1
Nareg04 <88930941+Nareg04@users.noreply.github.com>
2021-09-11 21:06:00 +0400

    one_if

--- main.cpp
+++ main.cpp
@@ -1,18 +1,14 @@
 #include "iostream"
 
-int main() {
-    int n  = 5;
-    for (int i = 1; i<=n; i++)
-    {
-        for(int k = n - i; k > 0; k--)
-        {
-            std::cout << " ";
-        }
-        for(int j = 1; j <= i; j++)
-        {
-            std::cout << "* ";
-        }
-        std::cout << std::endl;
+void test(int a , int b){
+    if(a < b && a % 2 != 0){
+        std::cout << a <<"\n";
     }
-    return  0;
+    a++;
+    test(a,b);
+}
+int main() {
+
+    test(10,23);
+    return 0;
 }
